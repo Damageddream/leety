8880fa177ff0b7854d80e7212f0592f7fc760c77
Damageddream <marcan.grab@gmail.com>
2024-07-21 18:02:48 +0200

    new leety

--- src/main/java/org/example/MaximumPositive.java
+++ src/main/java/org/example/MaximumPositive.java
@@ -2,20 +2,26 @@ package org.example;
 
 public class MaximumPositive {
     public static void main(String[] args) {
+        System.out.println(maximumCount(new int[]{1,4,8,9,11,15,13}));
 
     }
 
     public static int maximumCount(int[] nums) {
+        int target = 9;
         int left = 0;
         int right = nums.length - 1;
-        int mid = (left + right) / 2;
-        int neg = 0;
-        int pos = 0;
-        while(left <= right){
-
+        while (left <= right){
+            int mid = (left+right)/2;
             int temp = nums[mid];
-
+            if(target == temp){
+                return mid;
+            }
+            if(target < mid){
+                right = mid - 1;
+            } else{
+                left = mid + 1;
+            }
         }
-        return 0;
+        return -1;
     }
 }
