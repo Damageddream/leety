0f7064c3e2b701dab3ad780fdff9be9160c8d186
Damageddream <marcan.grab@gmail.com>
2024-08-04 14:55:33 +0200

    new leety

---
+++ src/main/java/org/example/RelativeRanks.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class RelativeRanks {
+    public static void main(String[] args) {
+
+    }
+    public static String[] findRelativeRanks(int[] score) {
+        return new String[]{};
+    }
+}
