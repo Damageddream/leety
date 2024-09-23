e39d3ab59a1deb9aeae66655d97b72143a7eceb7
Damageddream <marcan.grab@gmail.com>
2024-09-23 10:57:18 +0200

    new leety

---
+++ src/main/java/org/example/LargetNumInString.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.math.BigInteger;
+
+public class LargetNumInString {
+    public static void main(String[] args) {
+        System.out.println(largestOddNumber("52"));
+        System.out.println(largestOddNumber("4206"));
+        System.out.println(largestOddNumber("35427"));
+        System.out.println(largestOddNumber("239537672423884969653287101"));
+
+    }
+
+    public static String largestOddNumber(String num) {
+        int lastOddNumIndex = -1;
+        for (int i = 0; i < num.length(); i++) {
+            if (i == num.length() - 1 && Integer.parseInt(String.valueOf(num.charAt(i))) % 2 != 0) {
+                return num;
+            }
+            if (Integer.parseInt(String.valueOf(num.charAt(i))) % 2 != 0) {
+                lastOddNumIndex = i;
+            }
+        }
+        return lastOddNumIndex == -1 ? "" : num.substring(0, lastOddNumIndex + 1);
+    }
+}
